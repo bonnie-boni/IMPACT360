a9906febe68e0d1cc0b748bce8a20645d92d1563
Carla <carlacheruto3@gmail.com>
2025-03-20 09:57:37 +0300

    Added Home page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,6 @@
       <img src="../assets/logo.png" alt="Impact logo" />
     </div>
     <div class="nav__menu">
-        
       <ul>
         <li><router-link to="#">Home</router-link></li>
         <li><router-link to="#">About</router-link></li>
@@ -18,23 +17,32 @@
 </template>
 
 <script>
-export default {
-  name: 'AppHeader',
-}
+
 </script>
 <style scoped>
+/* base styles */
+* {
+    padding: 0px;
+    margin: 0px;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+    font-family: Poppins;
+    transition: all 0.3s ease-in-out;
+    color: black;
+}
 .header {
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: white;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-  position: fixed;
-  width :100%;
+  position: relative;
+  width: 100%;
   height: 70px;
   border-radius: 10px;
-
 }
+
 .nav__logo img {
   width: 70px;
   padding: 1rem;
@@ -45,16 +53,20 @@ export default {
   justify-content: space-between;
   padding: 1rem 2rem;
   gap: 0.5rem;
+  text-decoration: none;
 }
 .nav__menu ul li {
   padding: 0.5rem 1rem;
   border-radius: 0.5rem;
+  /* text-decoration: none; */
+  color: black;
   transition: all 0.5s ease-in-out;
 }
-.nav__menu ul li:hover {
-  /* background-color: rgb(12, 111, 224); */
+
+.nav__menu ul li :hover{
+  color: #2e6dea;
   opacity: 0.8;
-  color: #2E6DEA;
   cursor: pointer;
 }
+
 </style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero">
+    <div class="hero__content">
+      <h6>Together For Change</h6>
+      <h1>IMPACT 360</h1>
+      <p>BRINGING TECHNOPRENEURSHIP EVENTS AND RESOURCES AND RESOURCES TO YOUR NEAREST TOWN</p>
+      <button>JOIN I3 LAUNCHPAD</button>
+      <button>EVENTS</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
+
+<style scoped>
+.hero {
+  margin-top: 4px;
+}
+</style>
